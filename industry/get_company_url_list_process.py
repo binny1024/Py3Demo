ffffff3b73f2615e6b6842cda90d1a51b9112e9a
binny1024 <596928539@qq.com>
2020-04-30 09:38:30 +0800

    运行平台--linux

--- industry/get_company_url_list_process.py
+++ industry/get_company_url_list_process.py
@@ -4,9 +4,12 @@ import os
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
-from industry.industry import get_company_url_list_process
+# from industry.industry import get_company_url_list_process
+
+import industry.industry as ii
+
 """
 获取上市公司的简介连接
 """
 if __name__ == '__main__':
-    get_company_url_list_process()
+    ii.get_company_url_list_process()
